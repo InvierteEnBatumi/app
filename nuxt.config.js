6b0652ea69e255fc589064053441c9b6b2606991
brupadiro <brunodiharce@gmail.com>
2023-12-11 11:34:41 -0300

    ggg

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,7 +77,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://batumi-backend.onrender.com/api', 
+    baseURL: 'https://batumi-backend.app.brupadev.tech/api', 
   },
   css: [
     '~/assets/style.scss'
